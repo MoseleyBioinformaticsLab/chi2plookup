67cddc487083b9dc8a411f2d52b5367054452046
smelandr <smelandr@gmail.com>
2017-03-02 21:58:06 -0500

    Command-line interface update.

--- chi2plookup/__main__.py
+++ chi2plookup/__main__.py
@@ -12,12 +12,11 @@ Usage:
 Options:
     -h, --help                    Show this screen.
     --version                     Show version.
-    --verbose                     Print what files are processing.
+    --verbose                     Print more information.
     --headerfile=<path>           Path where to save generated header file [default: Chi2PValues.h]
     --precision=<precision>       Parameter that controls precision [default: 10000].
     --df=<df>                     Degrees of freedom, how many to ganarate [default: 6].
     --start_chi=<start_chi>       Maximum chi2 value for given degree of freedom [default: 25].
-    --headerfile_path=<template>  Header file template [default: Chi2PValues.hpp].
 """
 
 from . import docopt
